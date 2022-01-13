dbb8aa3db23f227642103aeb241aff4c30e294c8
small-j <44703262+small-j@users.noreply.github.com>
2022-01-13 23:20:37 +0900

    feat: main page에 Banner추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import Banner from "components/main/banner";
 import Share from "components/main/Share";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <>
+      <Banner />
       <Share />
     </>
   );
