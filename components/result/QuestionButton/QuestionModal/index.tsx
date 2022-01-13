675ddbd9890d05b6362da5b0f179235d0a10beca
small-j <44703262+small-j@users.noreply.github.com>
2022-01-13 17:41:51 +0900

    feat: 파일 생성

---
+++ components/result/QuestionButton/QuestionModal/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QuestionModal = function () {
+  return <div></div>;
+};
+
+export default QuestionModal;
---
+++ components/result/QuestionButton/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QuestionButton = function () {
+  return <div></div>;
+};
+
+export default QuestionButton;
