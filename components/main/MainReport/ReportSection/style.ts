602b73481e02e305a1470524f0e5f23174fefd2d
Hennization <shy6295@ewhain.net>
2022-01-13 23:30:44 +0900

    feat: StyledRoot section으로 수정

--- components/main/MainReport/ReportSection/style.ts
+++ components/main/MainReport/ReportSection/style.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { colors } from "styles/color";
 
-const StyledRoot = styled.div`
+const StyledRoot = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
