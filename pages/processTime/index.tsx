5ca5aa715a5fff0b6bd6b56ca4292fb2ae97ab28
JiyoungHyun <shyunji023@gmail.com>
2022-01-15 02:39:08 +0900

    feat: processTag 퍼블리싱

--- pages/processTime/index.tsx
+++ pages/processTime/index.tsx
@@ -1,18 +1,27 @@
+import CardTitle from "components/process/CardTitle";
+import Title from "components/process/Title";
 import Link from "next/link";
 
-import { ChoiceCard, Container, StyledRoot, Title } from "./style";
+import { CardChoice, ProcessBox, StyledRoot, TimeWrapper, TypeButton } from "./style";
 function ProcessTime() {
+  const timeTypeList = ["긴 강의", "짧은 강의", "상관없음"];
+
   return (
     <StyledRoot>
-      <Container>
-        <Title>
-          <Link href="/" passHref>
-            <p>&lt; 뒤로가기</p>
-          </Link>
-          <h2>맞춤 강의 찾기</h2>
-        </Title>
-        <ChoiceCard></ChoiceCard>
-      </Container>
+      <ProcessBox>
+        <Title></Title>
+        <CardTitle></CardTitle>
+        <CardChoice>
+          <TimeWrapper>
+            <p>
+              강의 <span>총 완강 시간</span>은 어떤 타입을 선호하시나요?
+            </p>
+            {timeTypeList.map((timeType, index) => (
+              <TypeButton key={index}>{timeType}</TypeButton>
+            ))}
+          </TimeWrapper>
+        </CardChoice>
+      </ProcessBox>
     </StyledRoot>
   );
 }
--- pages/processTime/style.ts
+++ pages/processTime/style.ts
@@ -1,32 +1,49 @@
 import styled from "styled-components";
 import { colors } from "styles/colors";
 export const StyledRoot = styled.section`
-  width: 100vw;
-  height: 100vh;
-  background: ${colors.gray3};
-`;
-export const Title = styled.div`
-  display: flex;
-  margin-top: 8.2rem;
-  align-items: center;
-  color: white;
-  font-family: "Pretendard-Bold";
-  font-size: 1.8rem;
-  & > h2 {
-    font-family: "Pretendard-Bold";
-    font-size: 4rem;
-    position: absolute;
-    left: calc(50% - 12.5rem);
-  }
-`;
-export const Container = styled.div`
-  width: 102rem;
-  position: relative;
-  margin: 0 auto;
-  display: flex;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to right, ${colors.mainBlue}, ${colors.subPink});
 `;
 
-export const ChoiceCard = styled.section`
+export const ProcessBox = styled.div`
+  position: relative;
   width: 102rem;
-  height: 66rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CardChoice = styled.section`
+  width: 102rem;
+  height: 49.3rem;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0 0 2.8rem 2.8rem;
+  border: 2px solid white;
+  margin-bottom: 14.2rem;
+`;
+
+export const TimeWrapper = styled.div`
+  width: 77.4rem;
+  margin: 6.5rem auto 0;
+  & > p {
+    font-family: "Pretendard-Bold";
+    font-size: 2.4rem;
+    color: ${colors.subBlack};
+    margin-bottom: 2rem;
+  }
+  & > p > span {
+    color: ${colors.subOrange};
+  }
+`;
+
+export const TypeButton = styled.button`
+  width: 22rem;
+  height: 18rem;
+  border-radius: 2.8rem;
+  font-family: "Pretendard-Bold";
+  font-size: 2.4rem;
+  color: ${colors.subBlack};
+  background-color: rgba(255, 255, 255, 0.7);
+  margin: 6.8rem 1.8rem 0;
 `;
