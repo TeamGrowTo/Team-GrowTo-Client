5c21253de6746e3a83348fc3f6be2b7409365c13
small-j <44703262+small-j@users.noreply.github.com>
2022-01-14 19:15:42 +0900

    fix: eslint 에러 해결

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -1,5 +1,5 @@
-export { default as EmailIcon } from "./emailIcon.svg";
-export { default as QuestionIcon } from "./questionIcon.svg";
 export { default as DownArrowIcon } from "./DownArrowIcon.svg";
-export { default as UpArrowIcon } from "./UpArrowIcon.svg";
+export { default as EmailIcon } from "./emailIcon.svg";
 export { default as LogoIcon } from "./logoIcon.svg";
+export { default as QuestionIcon } from "./questionIcon.svg";
+export { default as UpArrowIcon } from "./UpArrowIcon.svg";
