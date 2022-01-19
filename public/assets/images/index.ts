76d308975cbbbce5fc5900fc8c052313e9226103
Hennization <shy6295@ewhain.net>
2022-01-19 16:55:33 +0900

    chore: background 이미지 추가

--- public/assets/images/index.ts
+++ public/assets/images/index.ts
@@ -1,2 +1,3 @@
 export { default as MainBannerCompass } from "./mainBannerCompass.png";
+export { default as RequestLectureBackground } from "./requestLectureBackground.png";
 export { default as ReviewProfile } from "./reviewProfile.svg";
