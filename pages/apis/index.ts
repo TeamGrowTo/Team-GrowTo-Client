5154de8a439e8d273268561572be4fb0165f3e26
JiyoungHyun <shyunji023@gmail.com>
2022-01-20 12:17:54 +0900

    feat: api 파일 변경

--- pages/apis/index.ts
+++ pages/apis/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-//임시 url
+
 export const serverAxios = axios.create({
-  baseURL: "http://localhost:5005/",
+  baseURL: `${process.env.NEXT_PUBLIC_SERVER_URL}`,
+  withCredentials: true,
 });
