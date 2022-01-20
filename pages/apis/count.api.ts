7ee41946a6d6221a7fa46e13418cc41928cbe829
Hennization <shy6295@ewhain.net>
2022-01-21 04:52:30 +0900

    feat: count.api.ts 수정

--- pages/apis/count.api.ts
+++ pages/apis/count.api.ts
@@ -6,11 +6,13 @@ const PREFIX_URL = "/count";
 
 export const getLectureTotalNumber = async (): Promise<number | null> => {
   try {
-    const { data } = await serverAxios.get(`${PREFIX_URL}/total`);
-
-    return data((response: ResponseTotalNumber) => {
-      return { lectureTotalNumber: response.totalNumber };
+    const { data } = await serverAxios.get(`${PREFIX_URL}/total`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
+
+    return data.data;
   } catch (err) {
     throw new Error("Failed to load lecture total number");
   }
@@ -18,11 +20,13 @@ export const getLectureTotalNumber = async (): Promise<number | null> => {
 
 export const getLectureFindNumber = async (): Promise<number | null> => {
   try {
-    const { data } = await serverAxios.get(`${PREFIX_URL}/find`);
-
-    return data((response: ResponseFindNumber) => {
-      return { lectureFindNumber: response.findNumber };
+    const { data } = await serverAxios.get(`${PREFIX_URL}/find`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
+
+    return data.data;
   } catch (err) {
     throw new Error("Failed to load lecture find number");
   }
@@ -30,11 +34,13 @@ export const getLectureFindNumber = async (): Promise<number | null> => {
 
 export const getLectureRequestNumber = async (): Promise<number | null> => {
   try {
-    const { data } = await serverAxios.get(`${PREFIX_URL}/request`);
-
-    return data((response: ResponseRequestNumber) => {
-      return { lectureRequestNumber: response.requestNumber };
+    const { data } = await serverAxios.get(`${PREFIX_URL}/request`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
+
+    return data.data;
   } catch (err) {
     throw new Error("Failed to load lecture request number");
   }
