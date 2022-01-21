eae0d11f324d9a59338b0124f6890c5b3e9d3c6d
Hennization <shy6295@ewhain.net>
2022-01-22 03:57:05 +0900

    feat: mainReview 반응형 구현

--- public/assets/images/index.ts
+++ public/assets/images/index.ts
@@ -1,8 +1,14 @@
 export { default as MainBannerCompass } from "./mainBannerCompass.png";
 export { default as MainReviewBackground } from "./mainReviewBackground.png";
 export { default as MainReviewProfile1 } from "./mainReviewProfile1.svg";
+export { default as MainReviewProfile1_mobile } from "./mainReviewProfile1-mobile.svg";
 export { default as MainReviewProfile2 } from "./mainReviewProfile2.svg";
+export { default as MainReviewProfile2_mobile } from "./mainReviewProfile2-mobile.svg";
 export { default as MainReviewProfile3 } from "./mainReviewProfile3.svg";
+export { default as MainReviewProfile3_mobile } from "./mainReviewProfile3-mobile.svg";
 export { default as MainReviewProfile4 } from "./mainReviewProfile4.svg";
+export { default as MainReviewProfile4_mobile } from "./mainReviewProfile4-mobile.svg";
 export { default as MainReviewProfile5 } from "./mainReviewProfile5.svg";
+export { default as MainReviewProfile5_mobile } from "./mainReviewProfile5-mobile.svg";
 export { default as MainReviewProfile6 } from "./mainReviewProfile6.svg";
+export { default as MainReviewProfile6_mobile } from "./mainReviewProfile6-mobile.svg";
