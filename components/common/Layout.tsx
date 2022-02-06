751bca69e1e11cdddc64919eb399991ecbc9e62e
JiyoungHyun <shyunji023@gmail.com>
2022-02-06 17:18:32 +0900

    feat: process페이지 footer 삭제

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,4 +1,5 @@
 import Footer from "components/main/Footer";
+import { useRouter } from "next/router";
 import React from "react";
 
 import NavBar from "./NavBar";
@@ -6,11 +7,16 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 function Layout({ children }: LayoutProps) {
+  const router = useRouter();
+
+  const isProcess =
+    router.pathname === ("/processTag" || "/processTime" || "processTime") ? true : false;
+
   return (
     <>
       <NavBar />
       {children}
-      <Footer />
+      {isProcess ? <></> : <Footer />}
     </>
   );
 }
