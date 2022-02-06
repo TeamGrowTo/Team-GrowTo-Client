b62c2ea3dc7e21eb7653330e4dbe7ab597d17d37
small-j <44703262+small-j@users.noreply.github.com>
2022-02-06 19:54:08 +0900

    refactor: 리팩터링 및 svg 잘리는 오류 해결

--- components/result/MiddleNotification/style.ts
+++ components/result/MiddleNotification/style.ts
@@ -2,74 +2,69 @@ import styled from "styled-components";
 import { colors } from "styles/colors";
 import { applyMediaQuery } from "styles/mediaQuery";
 
-const StyledRoot = styled.div`
+export const StyledRoot = styled.section`
   display: flex;
   justify-content: center;
-  /* padding-bottom: 5.6rem; */
   flex-wrap: wrap;
   background-color: white;
   ${applyMediaQuery("mobile")} {
     padding: 0 1.6rem;
+  }
 `;
 
-const NotiBox = styled.div`
+export const NotiBox = styled.article`
   display: flex;
   align-items: center;
-  width: 84.6rem;
+  max-width: 84.6rem;
+  width: 100%;
   min-height: 10.4rem;
+  height: 100%;
   border: 0.1rem solid ${colors.gray3};
   border-radius: 2.8rem;
   margin-bottom: 5.6rem;
+  padding: 0 4.5rem 0 4.4rem;
   ${applyMediaQuery("mobile")} {
     width: 100%;
     height: auto;
     margin-bottom: 3.2rem;
-  }
-
-  & > svg {
-    width: 4rem;
-    height: 4rem;
-    margin: 0 1.2rem 0 4.4rem;
-    ${applyMediaQuery("mobile")} {
-      margin: 0 1.2rem 0 2rem;
-    }
+    padding: 0 2.4rem 0 2rem;
   }
 `;
 
-const NotiWrapper = styled.div`
+export const NotiWrapper = styled.section`
   display: flex;
   flex-direction: column;
   color: ${colors.gray6};
   font-size: 1.6rem;
   font-family: "Pretendard-Bold";
+  margin-left: 1.2rem;
   ${applyMediaQuery("mobile")} {
     font-size: 1.2rem;
-    margin-right: 2.4rem;
-    max-width: 23.2rem;
+    width: 100%;
   }
 `;
 
-const NotiTitle = styled.span`
+export const NotiTitle = styled.header`
   margin-bottom: 0.8rem;
   font-size: 1.6rem;
-  & > span {
-    color: ${colors.subOrange};
-  }
   ${applyMediaQuery("mobile")} {
     margin-bottom: 0.6rem;
     font-size: 1.2rem;
+    width: 100%;
   }
-`;
-
-const Content = styled.p`
-  line-height: 1.8rem;
-  font-size: 1.6rem;
   & > span {
     color: ${colors.subOrange};
   }
-  ${applyMediaQuery("mobile")} {
-    font-size: 1.2rem;
-  }
 `;
 
-export { Content, NotiBox, NotiTitle, NotiWrapper, StyledRoot };
+export const Content = styled.p`
+  line-height: 1.8rem;
+  font-size: 1.6rem;
+  ${applyMediaQuery("mobile")} {
+    font-size: 1.2rem;
+    width: 100%;
+  }
+  & > span {
+    color: ${colors.subOrange};
+  }
+`;
