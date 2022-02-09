352d7ea7a923981e40f1a873017842b880131b29
small-j <44703262+small-j@users.noreply.github.com>
2022-02-10 01:01:42 +0900

    feat: redirect bar가 자연스럽게 줄어들도록 변경

--- components/category/RedirectProcessButton/index.tsx
+++ components/category/RedirectProcessButton/index.tsx
@@ -3,30 +3,32 @@ import { ICRightIcon, ICRightSmallIcon, TenSecondIcon } from "public/assets/icon
 import React from "react";
 import Screen from "styles/Screen";
 
-import { Description, RedirectButton, StyledRoot } from "./style";
+import { Content, Description, RedirectButton, StyledRoot } from "./style";
 
 const RedirectProcessButton = function () {
   return (
     <StyledRoot>
-      <Description>
-        <Screen desktop>
-          <TenSecondIcon />
-        </Screen>
-        <span>단 10초면, 나에게 꼭 맞는 강의를 찾을 수 있어요.</span>
-      </Description>
-      <Link href="/processTag">
-        <RedirectButton>
-          <div>
-            <span>맞춤 강의 찾기</span>
-            <Screen desktop>
-              <ICRightIcon />
-            </Screen>
-            <Screen mobile>
-              <ICRightSmallIcon />
-            </Screen>
-          </div>
-        </RedirectButton>
-      </Link>
+      <Content>
+        <Description>
+          <Screen desktop>
+            <TenSecondIcon />
+          </Screen>
+          <span>단 10초면, 나에게 꼭 맞는 강의를 찾을 수 있어요.</span>
+        </Description>
+        <Link href="/processTag" passHref>
+          <RedirectButton>
+            <div>
+              <span>맞춤 강의 찾기</span>
+              <Screen desktop>
+                <ICRightIcon />
+              </Screen>
+              <Screen mobile>
+                <ICRightSmallIcon />
+              </Screen>
+            </div>
+          </RedirectButton>
+        </Link>
+      </Content>
     </StyledRoot>
   );
 };
