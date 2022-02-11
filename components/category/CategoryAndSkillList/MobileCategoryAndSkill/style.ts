a0a0bbeec4c0a37392f97a622fdfacf6958a990c
small-j <44703262+small-j@users.noreply.github.com>
2022-02-11 21:57:05 +0900

    fix: 모바일에서 화살표 이미지 크기 달라지는 오류 수정

--- components/category/CategoryAndSkillList/MobileCategoryAndSkill/style.ts
+++ components/category/CategoryAndSkillList/MobileCategoryAndSkill/style.ts
@@ -15,6 +15,7 @@ export const StyledRoot = styled.article`
 export const Category = styled.button`
   background-color: ${colors.mainBlue};
   width: 100%;
+  height: 100%;
   text-align: center;
   line-height: 5.6rem;
   color: ${colors.gray6};
@@ -22,11 +23,6 @@ export const Category = styled.button`
   padding: 0;
   cursor: pointer;
 
-  & > svg {
-    position: absolute;
-    top: 0;
-    right: 50%;
-  }
   & > span {
     width: 90%;
     height: 100%;
@@ -39,10 +35,20 @@ export const Category = styled.button`
 export const Skill = styled.button`
   background-color: ${colors.mainBlue};
   width: 100%;
+  height: 100%;
   text-align: center;
   line-height: 5.6rem;
   color: white;
   font-size: 1.4rem;
   font-family: "Pretendard-SemiBold";
   cursor: pointer;
+`;
+
+export const ImageWrapper = styled.div`
+  height: 100%;
+  & > svg {
+    position: absolute;
+    top: 0;
+    right: 50%;
+  }
 `;
