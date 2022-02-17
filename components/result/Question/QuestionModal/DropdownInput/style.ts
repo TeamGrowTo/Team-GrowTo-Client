525cb0ee334f4d74def75647cd0147194c26976d
small-j <44703262+small-j@users.noreply.github.com>
2022-02-17 16:29:30 +0900

    feat: 웹 드롭다운 리스트 hover 적용 완료

--- components/result/Question/QuestionModal/DropdownInput/style.ts
+++ components/result/Question/QuestionModal/DropdownInput/style.ts
@@ -5,10 +5,7 @@ import { applyMediaQuery } from "styles/mediaQuery";
 export const Dropdown = styled.article<{ flagDropdown: boolean }>`
   position: absolute;
   top: 9.1rem;
-  /* padding: 2.2rem 3.2rem; */
-  /* height: 16.8rem; */
   flex-direction: column;
-  /* justify-content: space-between; */
   font-size: 2rem;
   font-family: "Pretendard-Regular";
   color: ${colors.subBlack};
@@ -25,7 +22,6 @@ export const Dropdown = styled.article<{ flagDropdown: boolean }>`
     width: 100%;
     height: 5.4rem;
     padding: 0 3.05rem;
-    /* text-align: center; */
     line-height: 5.4rem;
     font-family: "Pretendard-Regular";
     font-size: 2rem;
@@ -39,21 +35,25 @@ export const Dropdown = styled.article<{ flagDropdown: boolean }>`
       width: 1.4rem;
     }
   }
-  & > button:focus {
-    border: 0.1rem solid ${colors.subBlack};
-  }
-  & > button:hover {
-    cursor: pointer;
-    background: ${colors.blue1};
-  }
   & > button:first-child {
     height: 5.6rem;
     line-height: 0;
+    padding-top: 2.1rem;
+    padding-bottom: 1.5rem;
   }
   & > button:last-child {
     height: 5.6rem;
     line-height: 0;
     border-radius: 0 0 2.8rem 2.8rem;
+    padding-top: 1.5rem;
+    padding-bottom: 2.1rem;
+  }
+  & > button:focus {
+    background: ${colors.blue1};
+  }
+  & > button:hover {
+    cursor: pointer;
+    background: ${colors.blue1};
   }
 
   ${({ flagDropdown }) =>
