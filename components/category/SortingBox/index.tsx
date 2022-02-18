e7369b2dc0e4954d75b6b6d997efbcfb26fa8cff
JisuKim <jso_osj@naver.com>
2022-02-19 01:13:33 +0900

    fix: skill클릭 시 sorting기준 유지 및 함수리팩토링

--- components/category/SortingBox/index.tsx
+++ components/category/SortingBox/index.tsx
@@ -23,7 +23,7 @@ import { SortingWrapper, StyledRoot } from "./style";
 //todo(4) : dropdown item선택하면 다른 선택된 item은 없애기 O
 //todo(5) : dropdown item선택하면 api 연결하기 O
 
-enum SortingText {
+export enum SortingText {
   //총 소요시간
   "긴 순" = "slow",
   "짧은 순" = "fast",
