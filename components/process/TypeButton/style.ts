32676facfc1aad6261754bf3911d716b1d085d39
JiyoungHyun <shyunji023@gmail.com>
2022-02-18 20:27:10 +0900

    style: type button 호버 효과

--- components/process/TypeButton/style.ts
+++ components/process/TypeButton/style.ts
@@ -13,6 +13,7 @@ export const StyledRoot = styled.button<{ isClicked: boolean }>`
   color: ${({ isClicked }) => (isClicked ? "white" : `${colors.gray6}`)};
   background: ${({ isClicked }) => (isClicked ? `${colors.mainBlue}` : "rgb(255, 255, 255, 0.7)")};
   border: 1px solid ${({ isClicked }) => (isClicked ? `${colors.mainBlue}` : "white")};
+  box-shadow: 4px 4px 8px rgba(53, 53, 53, 0.25);
   ${applyMediaQuery("mobile")} {
     font-size: 1.6rem;
     width: 22rem;
@@ -21,5 +22,8 @@ export const StyledRoot = styled.button<{ isClicked: boolean }>`
   }
   :hover {
     cursor: pointer;
+    background: ${colors.mainBlue};
+    border: 1px solid ${colors.mainBlue};
+    color: white;
   }
 `;
