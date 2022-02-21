0e804af0bf67a2a0e204cdeec009af02d2f49bf0
Hennization <shy6295@ewhain.net>
2022-02-21 23:36:33 +0900

    style: desktop 단위 수정

--- components/request/RequestLecture/style.ts
+++ components/request/RequestLecture/style.ts
@@ -5,22 +5,22 @@ import { applyMediaQuery } from "styles/mediaQuery";
 export const StyledRoot = styled.div`
   box-sizing: border-box;
   width: 100%;
-  height: 70rem;
+  height: 87.5rem;
   background: linear-gradient(to left, #9ba5ff, #466ed4);
   background-image: url("/assets/images/requestLectureBackground.png");
   display: flex;
   flex-direction: column;
   align-items: center;
   ${applyMediaQuery("mobile")} {
-    height: 48rem;
+    height: 60rem;
     background: linear-gradient(to top, #9aa4ff, #617fe2);
   }
 `;
 
 export const Wrapper = styled.div`
-  width: 88.6rem;
+  width: 110.5rem;
   height: 100%;
-  padding-top: 4.9rem;
+  padding-top: 6.125rem;
   position: relative;
   display: flex;
   flex-direction: column;
@@ -37,8 +37,8 @@ export const Title = styled.div`
 
   h2 {
     font-family: "Pretendard-SemiBold";
-    font-size: 4rem;
-    margin-bottom: 0.8rem;
+    font-size: 5rem;
+    margin-bottom: 1rem;
     ${applyMediaQuery("mobile")} {
       font-size: 2.2rem;
       margin-bottom: 0.6rem;
@@ -46,7 +46,7 @@ export const Title = styled.div`
   }
   p {
     font-family: "Pretendard-SemiBold";
-    font-size: 2.8rem;
+    font-size: 3.5rem;
     ${applyMediaQuery("mobile")} {
       font-size: 1.4rem;
     }
@@ -55,17 +55,21 @@ export const Title = styled.div`
 
 export const RequestCard = styled.div`
   width: 100%;
-  height: 54.6rem;
-  border-top-right-radius: 2.8rem;
-  border-top-left-radius: 2.8rem;
+  height: 68.25rem;
+  border-top-right-radius: 3.5rem;
+  border-top-left-radius: 3.5rem;
+  background: radial-gradient(
+    97.57% 210.75% at 0.9% 2.98%,
+    rgba(255, 255, 255, 0.4) 0%,
+    rgba(255, 255, 255, 0) 100%
+  );
   background-color: rgb(255, 255, 255, 0.65);
-  backdrop-filter: blur(10rem);
-  border: 0.3rem solid white;
+  backdrop-filter: blur(17.5rem);
+  border: 0.25rem solid white;
   position: absolute;
-  top: 15.4rem;
-  padding-top: 4.2rem;
+  top: 19.25rem;
+  padding-top: 5.25rem;
   ${applyMediaQuery("mobile")} {
-    width: 100%;
     height: 39.3rem;
     border: 0.2rem solid white;
     top: 8.8rem;
@@ -81,9 +85,9 @@ export const Form = styled.form`
 `;
 
 export const DisabledRequestButton = styled.button`
-  width: 63rem;
-  height: 6.8rem;
-  border-radius: 4.8rem;
+  width: 78.75rem;
+  height: 8.5rem;
+  border-radius: 6rem;
   background-color: ${colors.gray2};
   ${applyMediaQuery("mobile")} {
     width: 32.8rem;
@@ -92,7 +96,7 @@ export const DisabledRequestButton = styled.button`
 
   p {
     font-family: "Pretendard-Bold";
-    font-size: 2rem;
+    font-size: 2.5rem;
     color: ${colors.gray4};
     ${applyMediaQuery("mobile")} {
       font-size: 1.6rem;
@@ -101,9 +105,9 @@ export const DisabledRequestButton = styled.button`
 `;
 
 export const ActiveRequestButton = styled.button`
-  width: 63rem;
-  height: 6.8rem;
-  border-radius: 4.8rem;
+  width: 78.75rem;
+  height: 8.5rem;
+  border-radius: 6rem;
   background-color: ${colors.mainBlue};
   cursor: pointer;
   ${applyMediaQuery("mobile")} {
@@ -113,7 +117,7 @@ export const ActiveRequestButton = styled.button`
 
   p {
     font-family: "Pretendard-Bold";
-    font-size: 2rem;
+    font-size: 2.5rem;
     color: ${colors.white};
     ${applyMediaQuery("mobile")} {
       font-size: 1.6rem;
