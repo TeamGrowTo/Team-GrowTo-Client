e7e5ed4150a756d7dc79eae67c88550b1b10a33f
small-j <44703262+small-j@users.noreply.github.com>
2022-03-08 15:20:58 +0900

    fix: UX writing 변경으로 글자 밀림 해결

--- components/result/ProcessResult/style.ts
+++ components/result/ProcessResult/style.ts
@@ -42,7 +42,7 @@ export const Description = styled.p`
   text-align: center;
   ${applyMediaQuery("mobile")} {
     font-size: 1.4rem;
-    width: 20rem;
+    width: 22rem;
     height: auto;
     line-height: 1.8rem;
   }
